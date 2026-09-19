523887079000f3a6872ed456a09d047fd499df8f
NaomeP <naome.campbel@gmail.com>
2026-09-19 19:12:14 +0530

    Start Express server on Render

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,10 @@
 import app from "./app.js";
 
-export default app;
+const PORT = Number(process.env.PORT) || 5000;
+
+console.log("Starting server...");
+console.log("PORT =", PORT);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
