6547305c14553cdeee2bc303ebe0dde4024878b8
NaomeP <naome.campbel@gmail.com>
2026-09-19 17:24:11 +0530

    Test Vercel API routes

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,3 +1,16 @@
 import app from "../src/app";
 
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Vercel function is running",
+  });
+});
+
+app.get("/api/v1/health", (_req, res) => {
+  res.json({
+    success: true,
+    message: "Health route reached",
+  });
+});
+
 export default app;
